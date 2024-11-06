067b03c64d2717e40e02fee732a2339401a70a61
Fabio Paulo da Silva <fabioanatec@yahoo.com.br>
2024-11-06 11:13:38 -0300

    Meus Carros
    funcionando urls app chamando todas as views

--- cars/models.py
+++ cars/models.py
@@ -14,7 +14,9 @@ class Car(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=200, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self):
         return self.model
